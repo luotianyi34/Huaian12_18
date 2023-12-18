58bf20ba3cb119146425293cc66689fbe37172f8
wangtianrun <2602444171@qq.com>
2023-12-18 09:50:20 +0800

    default

--- src/demo01/UserInfo.java
+++ src/demo01/UserInfo.java
@@ -1,2 +1,20 @@
-package demo01;public class UserInfo {
+package demo01;
+
+public class UserInfo {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "UserInfo{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
--- src/demo01/demo.java
+++ src/demo01/demo.java
@@ -1,4 +1,29 @@
 package demo01;
 
-public class demo {
+public class demo extends Object{
+    //Object 类 超类 是所有类的默认父类
+
+    //finalize()
+    public static void main(String[] args) {
+        UserInfo userInfo = new UserInfo();
+        UserInfo userInfo2 = new UserInfo();
+        UserInfo userInfo3 = userInfo;
+        userInfo.setName("张三");
+        System.out.println(userInfo.hashCode());
+        userInfo2.setName("张三");
+        userInfo3.setName("李四");
+        String name = userInfo3.getName();
+        System.out.println(name);
+        System.out.println(userInfo2.hashCode());
+        System.out.println(userInfo3.hashCode());
+        System.out.println(userInfo2.equals(userInfo));
+        System.out.println(userInfo3.equals(userInfo));
+        Number n = 0;
+        Class<? extends Number> c = n.getClass();
+
+        //比较两个对象是否相等的时候 gethashCode();方法 两个引用对象如果get的值是一致的，那么，就说这两个对象相等（物理地址）
+
+        //equals方法 比的是值相同（物理地址的值）
+
+    }
 }
